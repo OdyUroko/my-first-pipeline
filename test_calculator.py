8fc51f01b43d3350b0d6ad8e31a3604a441399c4
OdyUroko <urokoodinakavivian@gmail.com>
2026-05-17 14:51:29 +0100

    testing applications

---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+# test_calculator.py
+
+from calculator import add, subtract, multiply
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+
+def test_subtract():
+    assert subtract(10, 4) == 6
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
